0a368852c0d18f2e1f1956f4cda60e49e13405b9
Zireael07 <zi@mail.com>
2019-12-07 14:08:34 +0100

    new: all the buttons now have click handlers

--- static/keypad.js
+++ static/keypad.js
@@ -15,4 +15,109 @@ $( document ).ready(function() {
         });
 
     });
+    $("#go_w").click(function(e) {
+
+        //console.log("Clicked a button 1");
+        $.ajax({
+            url: "/move/-1/0",
+            type: "GET",
+            success: function(response){
+                console.log("Success");
+                $('p').html(response.data);
+                //this does update the page but buttons no longer work
+                //document.write(html)
+            }
+        });
+
+    });
+    $("#go_e").click(function(e) {
+
+        //console.log("Clicked a button 1");
+        $.ajax({
+            url: "/move/1/0",
+            type: "GET",
+            success: function(response){
+                console.log("Success");
+                $('p').html(response.data);
+                //this does update the page but buttons no longer work
+                //document.write(html)
+            }
+        });
+
+    });
+    $("#go_se").click(function(e) {
+
+        //console.log("Clicked a button 1");
+        $.ajax({
+            url: "/move/1/1",
+            type: "GET",
+            success: function(response){
+                console.log("Success");
+                $('p').html(response.data);
+                //this does update the page but buttons no longer work
+                //document.write(html)
+            }
+        });
+
+    });
+    $("#go_sw").click(function(e) {
+
+        //console.log("Clicked a button 1");
+        $.ajax({
+            url: "/move/-1/1",
+            type: "GET",
+            success: function(response){
+                console.log("Success");
+                $('p').html(response.data);
+                //this does update the page but buttons no longer work
+                //document.write(html)
+            }
+        });
+
+    });
+    $("#go_n").click(function(e) {
+
+        //console.log("Clicked a button 1");
+        $.ajax({
+            url: "/move/0/-1",
+            type: "GET",
+            success: function(response){
+                console.log("Success");
+                $('p').html(response.data);
+                //this does update the page but buttons no longer work
+                //document.write(html)
+            }
+        });
+
+    });
+    $("#go_ne").click(function(e) {
+
+        //console.log("Clicked a button 1");
+        $.ajax({
+            url: "/move/1/-1",
+            type: "GET",
+            success: function(response){
+                console.log("Success");
+                $('p').html(response.data);
+                //this does update the page but buttons no longer work
+                //document.write(html)
+            }
+        });
+
+    });
+    $("#go_nw").click(function(e) {
+
+        //console.log("Clicked a button 1");
+        $.ajax({
+            url: "/move/-1/-1",
+            type: "GET",
+            success: function(response){
+                console.log("Success");
+                $('p').html(response.data);
+                //this does update the page but buttons no longer work
+                //document.write(html)
+            }
+        });
+
+    });
 });
