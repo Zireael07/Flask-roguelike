22a65a60c401d216e73b493ffbc0d7cea5b26c52
Zireael07 <zi@mail.com>
2019-12-21 14:28:19 +0100

    fix: noise that extends instead of zooming in

--- logic/noise_ext.py
+++ logic/noise_ext.py
@@ -2,15 +2,16 @@
 from . import noise
 
 # based on http://flafla2.github.io/2014/08/09/perlinnoise.html
-def octave_perlin(x,y, octaves=2, persistence=2):
+def octave_perlin(x,y, octaves=2, persistence=2, x_freq = 1, y_freq = 1, amplitude=1):
     total = 0
     #maxValue = 0 # used to normalize to 0-1 range
-    freq = 1
-    amplitude = 1
+    # freq = 1
+    # amplitude = 1
     for i in range(0, octaves):
-        total += noise.noise_2d(x*freq,y*freq) * amplitude
+        total += noise.noise_2d(x*x_freq,y*y_freq) * amplitude
         # black magic here!
         amplitude *= persistence
-        freq *= 2
+        x_freq *= 2
+        y_freq *= 2
     
     return total
--- logic/noisemap.py
+++ logic/noisemap.py
@@ -6,17 +6,27 @@ from . import noise_ext
 def map_create():
     new_map = [[ get_index(TileTypes.WALL) for _ in range(0, constants.MAP_HEIGHT)] for _ in range(0, constants.MAP_WIDTH)]
 
+    # perlin visualization
+    noise = [[ 0 for _ in range(0, constants.MAP_HEIGHT)] for _ in range(0, constants.MAP_WIDTH)]
+
 	# make actual map
     for x in range (constants.MAP_WIDTH):
         for y in range (constants.MAP_HEIGHT):
 
+            # this would scale the map the bigger it was, instead of continuing
             # black magic here!
-            i = x/constants.MAP_WIDTH
-            j = y/constants.MAP_HEIGHT
+            #i = x/constants.MAP_WIDTH
+            #j = y/constants.MAP_HEIGHT
+
+            # https://rtouti.github.io/graphics/perlin-noise-algorithm
+            # the inputs cannot be integers since that messes up the algorithm
+            i = x*0.01
+            j = y*0.01
 
             # noise
             #n = noise.noise_2d(i,j)
-            n = noise_ext.octave_perlin(i,j, 2, 3)
+            n = noise_ext.octave_perlin(i,j, 4, 2)
+            noise[x][y] = n
 
             # map
             if n < 0.45:
