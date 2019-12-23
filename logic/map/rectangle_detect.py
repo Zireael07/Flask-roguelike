34dc799b006572cbb70528ca7a39b745691b480b
Zireael07 <zi@mail.com>
2019-12-23 17:08:56 +0100

    fix: cleanup and minor fixes to rectangle detection

--- logic/map/rectangle_detect.py
+++ logic/map/rectangle_detect.py
@@ -74,49 +74,17 @@ def max_rectangle_area( histogram, hist_index):
             # if lower, pop from stack
             elif stack and height < top().height:
                 width = pos - top().start
-                area = top().height * width
+                area = height * width
                 if area > max_area:
                     max_area = area
                     # we assigned 1 for the first free cell in a column, but a Rect with a height of 1 is x+1 not x
                     # so we need to deduce 1 (so we get x and not x+1)
                     # this algo is bottom-up, rect is top-down - deduce height from y and add 1 so that we cover the bottom line
-                    res = Rect(pos-width, hist_index-top().height+1, width, height+1), hist_index
-                # this line goofed up max area for some histograms
-                #max_area = max(max_area, top().height * (pos - top().start))
+                    res = Rect(pos-width, hist_index-height-1, width, height+1), hist_index
+
                 start, _ = stack.pop()
                 continue
             break  # height == top().height goes here
-
-    # pos += 1
-    # for (start, height) in stack:
-    #     print("Pos " + str(pos) + " i: " + str(stack.index((start, height))))
-    #     width = (pos - start)
-    #     area = height * width
-    #     print("From stack: start:" + str(start) + " h: " + str(height) + " w: " + str(width) + " area: " + str(area))
-
-    #     max_area = max(max_area, area)
-
-    #     rects.append((start, height, width, area))
-
-    #print("Max area in line: " + str(max_area))
-
-    # # added by yours truly, TODO: can we do this somewhere earlier?
-    # for r in rects:
-    #     start, height, width, area = r
-    #     # print("From stack: start:" + str(start) + " h: " + str(height) + " w: " + str(width) + " area: " + str(area))
-    #     # are we the biggest? get additional info
-    #     if area == max_area:
-    #         # we want width and height and start position in addition to area
-    #         # i = stack.index((start, height))
-    #         # print("Biggest rectangle index: " + str(i) + " for y: " + str(hist_index))
-    #         print("Biggest for y: " + str(hist_index) + " start: " + str(start) + " h: " + str(
-    #             height) + " w: " + str(width) + " area: " + str(area))
-
-
-    #         # we assigned 1 for the first free cell in a column, but a Rect with a height of 1 is x+1 not x
-    #         # so we need to deduce 1 (so we get x and not x+1)
-    #         # this algo is bottom-up, rect is top-down - deduce height from y and add 1 so that we cover the bottom line
-    #         res = Rect(start, hist_index-height+1, width, height-1), hist_index
 
     return max_area, res
 
@@ -168,8 +136,9 @@ def run_rectangle_detection(mapa):
 def fill_big_rect(rect, mapa):
     #print("Filling... " + str(rect.x1) + "," + str(rect.y1) + ", w " + str(rect.x2-rect.x1) + " h " + str(rect.y2-rect.y1))
     # Set all tiles in rect to debug
-    for x in range(rect.x1, rect.x2):
-        for y in range(rect.y1, rect.y2):
+    # range is exclusive at the end!
+    for x in range(rect.x1, rect.x2+1):
+        for y in range(rect.y1, rect.y2+1):
             # it's just for debugging purposes
             mapa[x][y] = get_index(TileTypes.DEBUG)
 
