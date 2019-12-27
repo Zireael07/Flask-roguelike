f965a140f33d1beadb75d9d643ab7be8666b1d89
Zireael07 <zi@mail.com>
2019-12-27 10:03:27 +0100

    new: backend - NPCs drop their equipment on death

--- logic/processors/death_processor.py
+++ logic/processors/death_processor.py
@@ -5,6 +5,8 @@ from ..components.name_component import NameComponent
 from ..components.dead_component import DeadComponent
 from ..components.combat_stats import StatsComponent
 
+from ..components.equipped import EquippedComponent
+from ..components.position import Position
 
 from .. import game_vars
 
@@ -13,12 +15,21 @@ class DeathProcessor(esper.Processor):
         super().__init__()
 
     def process(self):
-        for ent, (name, dead) in self.world.get_components(NameComponent, DeadComponent):
+        for ent, (name, dead, pos) in self.world.get_components(NameComponent, DeadComponent, Position):
             if self.world.has_component(ent, Player):
                 #print("Player is dead!!!")
                 game_vars.messages.append(("YOU DIED!!", (255, 0, 0)))
                 # skip
                 continue
+
+            # drop their stuff
+            for item_ent, (equip) in self.world.get_component(EquippedComponent):
+                if equip.owner == ent:
+                    # drop
+                    self.world.remove_component(item_ent, EquippedComponent)
+            
+                    pos_item = self.world.component_for_entity(item_ent, Position)
+                    pos_item.x, pos_item.y = pos.x, pos.y
 
             # if fighter.hp > 0:
             #     # skip
